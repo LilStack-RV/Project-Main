7a570d26f2e5919f4fdbc2f5e3cb2e3b0582eefa
Natalie Russell <natalierussell@dhcp-v512-00188.resnet-1x-wireless.unc.edu>
2018-03-01 02:49:57 -0500

    updated styling

--- public/jscript/browse.js
+++ public/jscript/browse.js
@@ -26,22 +26,22 @@ var popCardsInitial = function(cards){
     if(i<3){
       $("#row-1").append('<div class="col-lg-4">' + '<div class="card" style="background-color: #404040;">'
     + '<div class="card-block">' + '<h3 class="card-header text-center text-light text-bold">' + cards[i].name + 'Card 1</h3>'
-    + '<img src="' + cards[i].image + '" style="width:20px; height:20px;">' + '<p class="card-text">' + cards[i].features[0] + '</p>'
-    + '<p class="card-text">' + cards[i].features[1] + '</p>' + '<p class="card-text">' + cards[i].features[2] + '</p>'
+    + '<img src="' + cards[i].image + '" style="width:327px; height:210px;">' + '<p class="card-text">' + cards[i].features[0] + '</p>'
+    + '<p class="card-text" style=>' + cards[i].features[1] + '</p>' + '<p class="card-text">' + cards[i].features[2] + '</p>'
     + '<p class="card-text">' + cards[i].features[3] + '</p>'
     + '<p class="card-text">' + Math.floor(cards[i].regular_apr.rate*100) + "% variable apr" + '</p>'
-    + '<div class="card-footer text-right">' + '<a href="#" class="btn btn-primary ">Apply</a>' + '</div>'
+    + '<div class="card-footer text-right">' + '<a href="#" class="btn btn-info text-white btn-block">Apply</a>' + '</div>'
     + '</div>'
     + '</div>'
     + '</div>');
     }else if(i<6){
       $("#row-2").append('<div class="col-lg-4">' + '<div class="card" style="background-color: #404040;">'
     + '<div class="card-block">' + '<h3 class="card-header text-center text-light text-bold">' + cards[i].name + 'Card 1</h3>'
-    + '<img src="' + cards[i].image + '" style="width:20px; height:20px;">' + '<p class="card-text">' + cards[i].features[0] + '</p>'
+    + '<img src="' + cards[i].image + '" style="width:327px; height:210px;">' + '<p class="card-text">' + cards[i].features[0] + '</p>'
     + '<p class="card-text">' + cards[i].features[1] + '</p>' + '<p class="card-text">' + cards[i].features[2] + '</p>'
     + '<p class="card-text">' + cards[i].features[3] + '</p>'
     + '<p class="card-text">' + Math.floor(cards[i].regular_apr.rate*100) + "% variable apr" + '</p>'
-    + '<div class="card-footer text-right">' + '<a href="#" class="btn btn-primary ">Apply</a>' + '</div>'
+    + '<div class="card-footer text-right">' + '<a href="#" class="btn btn-info text-white btn-block">Apply</a>' + '</div>'
     + '</div>'
     + '</div>'
     + '</div>');
@@ -49,11 +49,11 @@ var popCardsInitial = function(cards){
     }else if(i<9){
       $("#row-3").append('<div class="col-lg-4">' + '<div class="card" style="background-color: #404040;">'
     + '<div class="card-block">' + '<h3 class="card-header text-center text-light text-bold">' + cards[i].name + 'Card 1</h3>'
-    + '<img src="' + cards[i].image + '" style="width:20px; height:20px;">' + '<p class="card-text">' + cards[i].features[0] + '</p>'
+    + '<img src="' + cards[i].image + '" style="width:327px; height:210px;">' + '<p class="card-text">' + cards[i].features[0] + '</p>'
     + '<p class="card-text">' + cards[i].features[1] + '</p>' + '<p class="card-text">' + cards[i].features[2] + '</p>'
     + '<p class="card-text">' + cards[i].features[3] + '</p>'
     + '<p class="card-text">' + Math.floor(cards[i].regular_apr.rate*100) + "% variable apr" + '</p>'
-    + '<div class="card-footer text-right">' + '<a href="#" class="btn btn-primary ">Apply</a>' + '</div>'
+    + '<div class="card-footer text-right">' + '<a href="#" class="btn btn-info text-white btn-block">Apply</a>' + '</div>'
     + '</div>'
     + '</div>'
     + '</div>');
