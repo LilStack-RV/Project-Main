6444bcb42849307394b9b0b471db7d09aaedd41c
Andrew McKinnon <andrewmckinnon2@gmail.com>
2018-03-01 13:43:44 -0500

    Started to set up functionality for apply page

--- public/jscript/apply.js
+++ public/jscript/apply.js
@@ -2,7 +2,7 @@
   console.log(sessionStorage.getItem("card"));
 })*/
 var cards = []; //Going to be used to store cards for dynamic procesising of user searches
-
+var cardApplyingTo;
 $(document).ready(function(){
   //when document has loaded, make request for cards
   var getCardsUrl = "https://techcase-cards-api.herokuapp.com/api/v1/cards"
@@ -20,7 +20,13 @@ var handleCards = function(data){
   data.forEach(function(card){
     cards.push(card);
   });
-  console.log(cards);
+  for(var i=0; i<cards.length; i++){
+    if(cards[i].name == sessionStorage.getItem("card")){
+      cardApplyingTo = cards[i];
+      $("#image").attr("src", cardApplyingTo.image);
+      $("#card-name").append(" " + cardApplyingTo.name);
+    }
+  }
 }
 
 $("#searchbar").keyup(function(e){
@@ -50,4 +56,26 @@ $(document).on("mousedown", "p.cardLink", function(){
   console.log($(this).html());
   sessionStorage.setItem("card", $(this).html());
   window.location = "apply.html";
+})
+
+$("#submit-button").click(function(){
+  var firstName = $("#firstName").val();
+  var lastName = $("#lastName").val();
+  var dob = $("#DOB").val();
+  var creditScore = $("#creditScore").val();
+  var income = $("#income").val();
+  var streetAddress = $("#street_address").val();
+  var city = $("#city").val();
+  var state = $("#state").val();
+  var zip = $("#zip").val();
+  var email = $("#email").val();
+  var phoneNum = $("#phone_number").val();
+
+  if(firstName == undefined || lastName == undefined || dob == undefined ||
+  creditScore == undefined || income == undefined || streetAddress == undefined ||
+  city == undefined || state == undefined || zip == undefined || email == undefined || phoneNum == undefined){
+    alert("Please check to make sure all fileds are filled in before applying");
+  }else{
+
+  }
 })
